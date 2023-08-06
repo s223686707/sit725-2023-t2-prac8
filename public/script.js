5e975266b742dc9a26ee5e9c67b904f3da387c8b
s223686707 <s223686707@deakin.edu.au>
2023-08-07 02:36:34 +1000

    DB added

--- public/script.js
+++ public/script.js
@@ -1,23 +1,5 @@
-const cardList = [{
-    title: 'Subway',
-    image: '/images/sub.jpeg',
-    subTitle: 'Subway is known for its made-to-order submarine sandwiches (subs) and salads.Subway is known for its made-to-order submarine sandwiches (subs) and salads.Subway is known for its made-to-order submarine sandwiches (subs) and salads.',
-    description: 'Subway is known for its made-to-order submarine sandwiches (subs) and salads.Customers can choose from various bread, protein, vegetable, and sauce options.Burger King is another global fast-food chain famous for its flame-grilled burgers. They offer a diverse menu with items like the Whopper (their signature burger), chicken sandwiches, salads, and a variety of sides.'
-},
-{
-    title: 'Burger King',
-    image: '/images/king.png',
-    subTitle: 'Burger King is another global fast-food chain famous for its flame-grilled burgers. They offer a diverse menu with items like the Whopper (their signature burger), chicken sandwiches, salads, and a variety of sides.',
-    description: 'Burger King is another global fast-food chain famous for its flame-grilled burgers. They offer a diverse menu with items like the Whopper (their signature burger), chicken sandwiches, salads, and a variety of sides.'
-},
-{
-    title: 'Pizza Hut',
-    image: '/images/download.png',
-    subTitle: 'Pizza Hut is another popular pizza restaurant chain, offering a variety of pizzas, pasta, wings, and desserts. They have both dine-in and delivery options.Pizza Hut is another popular pizza restaurant chain, offering a variety of pizzas, pasta, wings, and desserts. They have both dine-in and delivery options.',
-    description: 'Pizza Hut is another popular pizza restaurant chain, offering a variety of pizzas, pasta, wings, and desserts. They have both dine-in and delivery options.Pizza Hut is another popular pizza restaurant chain, offering a variety of pizzas, pasta, wings, and desserts. They have both dine-in and delivery options.Pizza Hut is another popular pizza restaurant chain, offering a variety of pizzas, pasta, wings, and desserts. They have both dine-in and delivery options.'
-}];
-
-const addCards = (items) => {
+const addItems = (items) => {
+    console.log(items);
     items.forEach(item => {
         let itemToAppend = '<div class="col s4 center-align">'+
                 '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
@@ -25,16 +7,27 @@ const addCards = (items) => {
                 '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.subTitle+'</a></p></div>'+
                 '<div class="card-reveal">'+
                 '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-                '<p class="card-text">'+item.desciption+'</p>'+
+                '<p class="card-text">'+item.description+'</p>'+
                 '</div></div></div>';
         $("#card-section").append(itemToAppend)
     });
 }
 
+function getAllItems(){
+    $.get('/getItems', (res)=>{
+        if(res.statusCode === 200 ){
+            console.log(res.data);
+            addItems(res.data);
+        }
+    });
+}
 
 $(document).ready(function(){
     $('.materialboxed').materialbox();
-    addCards(cardList);
     $('.modal').modal();
+    getAllItems();
+    
 });
+
+
   
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:admin@cluster0.h6nrkl7.mongodb.net/?retryWrites=true&w=majority";
 const bodyParser = require('body-parser');
+let collections;
 
 const app = express();
 
@@ -7,30 +10,73 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function runMongoConnection() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    collections = client.db().collection('Sections');
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+
 app.get('/', function (req,res) {
     res.render('index.html');
 });
 
-app.post('/order',(req,res)=>{
-    const name = req.body.name
-    const address = req.body.delivery_address
-    const qty = req.body.quantity
-    const phoneNumber = req.body.phone_number
-    
-    const orders = {
-        name: name,
-        delivery_address: address,
-        quantity: qty,
-        mobile_number: phoneNumber,
-      }
-    
-    console.log(orders);  
-    res.json(orders);
+app.get('/getItems', (req, res) => {
+    getAllItems((err, result) => {
+        if (!err){
+            res.json({statusCode:200, data:result, message: 'Get All items Successfully!!'});
+        }
+    });
+});
 
+app.post('/order', (req,res)=>{
 
-})
+    const title = req.body.title;
+    const image = req.body.image;
+    const subTitle = req.body.subtitle;
+    const description = req.body.description;
+
+    let newFood = {
+        title: title,
+        image: image,
+        subTitle: subTitle,
+        description: description
+    }
+    
+    insertItem(newFood);
+
+});
+
+function getAllItems(callback){
+    collections.find({}).toArray(callback);
+}
+
+function insertItem(newFood){
+    collections.insertOne(newFood, (err, result) => {
+        if(err){
+            console.error("Error in Inserting Item in DB", err);
+        }else{
+            console.log("Inserted into DB Successfully!!");
+        }
+    });
+}
 
 
 app.listen(3000, (req,res)=>{
     console.log("Server is running");
-})
+    runMongoConnection().catch(console.dir);
+});
